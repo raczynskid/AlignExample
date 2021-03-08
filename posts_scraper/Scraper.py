3d8af1927d2a264cfa8d2306d448e00ae8cd7c72
Kiermasz <raczynski90@gmail.com>
2021-03-08 02:52:02 +0100

    twitter api scraping functional

--- posts_scraper/Scraper.py
+++ posts_scraper/Scraper.py
@@ -1,4 +1,5 @@
 from posts_scraper.api_access.pushshift_wrapper import PushshiftScraper
+from posts_scraper.api_access.twitter_api_wrapper import TwitterScraper
 
 ps = PushshiftScraper(queries=["invisalign"], days=10, interval=6)
-print(ps.data)
+ts = TwitterScraper("invisalign").results_to_dataframe()
