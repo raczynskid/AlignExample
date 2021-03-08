f645b53f2e76ccbd354a7afa42686cdeadbab2ce
Kiermasz <raczynski90@gmail.com>
2021-03-08 01:09:36 +0100

    reddit scrapper functional

--- posts_scraper/Scraper.py
+++ posts_scraper/Scraper.py
@@ -1,33 +1,4 @@
-import praw
-from posts_scraper.data.list_of_subreddits import sport_subreddit_list
-from requests import Session
-session = Session()
-session.verify = r"C:\Users\Kiermasz\Align\Lib\site-packages\requests\cacert.pem"
-reddit = praw.Reddit(
-    client_id="K7o_XVRtuuLztA",
-    client_secret="oik01__3RhseTNOqgMgHXG5KPnjkdQ",
-    requestor_kwargs={"session": session},  # pass Session
-    user_agent="data scraper for align interview",
-)
+from posts_scraper.api_access.pushshift_wrapper import PushshiftScraper
 
-subs = [reddit.subreddits.search_by_name(sub_name, include_nsfw=False) for sub_name in sport_subreddit_list]
-
-for group in subs[:10]:
-    for sub in group:
-        threads = sub.top(limit=100)
-        for thread in threads:
-            thread.comments.replace_more(limit=0)
-            comments_of_interest = [(thread.title, comment.body)
-                                    for comment in thread.comments
-                                    if ("teeth" in comment.body.lower())
-                                    or ("tooth" in comment.body.lower())
-                                    or ("teeth" in thread.title.lower())]
-            if len(comments_of_interest) > 0:
-                print(comments_of_interest)
-
-def get_child_posts():
-    url = "https://www.reddit.com/r/AskScienceFiction/comments/lyf6mk/lotrwhat_did_gandalf_exactly_do_when_he_hit_the/gpshko6?utm_source=share&utm_medium=web2x&context=3"
-    submission = reddit.submission(url=url)
-
-    for top_level_comment in submission.comments:
-        print(top_level_comment.body)
+ps = PushshiftScraper(queries=["invisalign"], days=10, interval=6)
+print(ps.data)
---
+++ posts_scraper/api_access/pushshift_wrapper.py
@@ -0,0 +1,51 @@
+import requests
+import pandas as pd
+import json
+import time
+import datetime
+
+class PushshiftScraper:
+    def __init__(self, **kwargs):
+        self.data = self.convert_dates(self.paginate_by_utc(**kwargs)).sort_values(
+                                                                                                    by="created_utc",
+                                                                                                    ascending=False)
+
+    def paginate_by_utc(self, queries: list, days: int, interval: int) -> pd.DataFrame:
+        """
+        due to change in pushshift api max return limit need to paginate by comment add date
+        :param queries: list of strings to be included in the search eg ["invisalign", "teeth"]
+        :param days: period in days to search for
+        :param interval: data granularity in hours (paginate by x hours)
+                         example: paginate_by_utc(["invisalign"], 10, 3) - find invisalign posts for last 10 days
+                                                                           with 3h granularity to avoid going over
+                                                                           100 comments api limit - increase up to 24 to
+                                                                           improve performance, decrease to improve
+                                                                           data quality
+        :return: dataframe containing concatenated api response
+        """
+        data_chunks = []
+        # cycle through queries
+        for query in queries:
+            after = interval
+
+            # for each query take 1 day period of comments and move on
+            for _ in range((24//interval) * days):
+                before = after - interval
+                url = f"https://api.pushshift.io/reddit/comment/search/?q={query}&after={after}h&before={before}h&sort=asc&size=100&fields=author,author_flair_text,body,comment_type,created_utc,id,link_id,permalink,score,subreddit,subreddit_id"
+                df = pd.json_normalize(json.loads(requests.get(url).text), record_path='data')
+                time.sleep(1)
+                if len(df) > 0:
+                    print(len(df))
+                    data_chunks.append(df)
+
+                after += interval
+
+        result = pd.concat(data_chunks).reset_index(drop=True)
+        return result
+
+    def convert_dates(self, df: pd.DataFrame) -> pd.DataFrame:
+        df["created_utc"] = df["created_utc"].apply(lambda x: datetime.datetime.fromtimestamp(x))
+        return df
+
+
+
